16c4699a96e82299b2402e9243697232e7161fc3
reneeb <info@perl-services.de>
2017-11-17 14:34:34 +0100

    initial commit

---
+++ Kernel/System/DynamicField/Driver/MultiCheckbox.pm
@@ -0,0 +1,172 @@
+# --
+# Copyright (C) 2017 Perl-Services.de, http://perl-services.de
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::System::DynamicField::Driver::MultiCheckbox;
+
+use strict;
+use warnings;
+
+use Kernel::System::VariableCheck qw(:all);
+
+use base qw(Kernel::System::DynamicField::Driver::Multiselect);
+
+our @ObjectDependencies = (
+    'Kernel::Config',
+    'Kernel::System::DynamicFieldValue',
+    'Kernel::System::Log',
+    'Kernel::System::Main',
+);
+
+=head1 NAME
+
+Kernel::System::DynamicField::Driver::MultiCheckbox
+
+=head1 SYNOPSIS
+
+DynamicFields MultiCheckbox Driver delegate
+
+=head1 PUBLIC INTERFACE
+
+This module implements the public interface of L<Kernel::System::DynamicField::Backend>.
+Please look there for a detailed reference of the functions.
+
+=over 4
+
+=item new()
+
+usually, you want to create an instance of this
+by using Kernel::System::DynamicField::Backend->new();
+
+=cut
+
+sub EditFieldRender {
+    my ( $Self, %Param ) = @_;
+
+    # take config from field config
+    my $FieldConfig = $Param{DynamicFieldConfig}->{Config};
+    my $FieldName   = 'DynamicField_' . $Param{DynamicFieldConfig}->{Name};
+    my $FieldLabel  = $Param{DynamicFieldConfig}->{Label};
+
+    my $Value;
+
+    # set the field value or default
+    if ( $Param{UseDefaultValue} ) {
+        $Value = ( defined $FieldConfig->{DefaultValue} ? $FieldConfig->{DefaultValue} : '' );
+    }
+    $Value = $Param{Value} // $Value;
+
+    # check if a value in a template (GenericAgent etc.)
+    # is configured for this dynamic field
+    if (
+        IsHashRefWithData( $Param{Template} )
+        && defined $Param{Template}->{$FieldName}
+        )
+    {
+        $Value = $Param{Template}->{$FieldName};
+    }
+
+    # extract the dynamic field value form the web request
+    my $FieldValue = $Self->EditFieldValueGet(
+        %Param,
+    );
+
+    # set values from ParamObject if present
+    if ( IsArrayRefWithData($FieldValue) ) {
+        $Value = $FieldValue;
+    }
+
+    # check and set class if necessary
+    my $FieldClass = 'DynamicFieldText Modernize';
+    if ( defined $Param{Class} && $Param{Class} ne '' ) {
+        $FieldClass .= ' ' . $Param{Class};
+    }
+
+    # set field as mandatory
+    if ( $Param{Mandatory} ) {
+        $FieldClass .= ' Validate_Required';
+    }
+
+    # set error css class
+    if ( $Param{ServerError} ) {
+        $FieldClass .= ' ServerError';
+    }
+
+    # set PossibleValues, use PossibleValuesFilter if defined
+    my $PossibleValues = $Param{PossibleValuesFilter} // $Self->PossibleValuesGet(%Param);
+
+    my %SelectedValues;
+    if ( defined $Value ) {
+        if ( ref $Value eq 'ARRAY' ) {
+            for my $SingleValue ( @{ $Value } ) {
+                $SelectedValues{$SingleValue} = 1;
+            }
+        }
+        else {
+            $SelectedValues{$Value} = 1;
+        }
+    }
+
+    my $DataValues = $Self->BuildSelectionDataGet(
+        DynamicFieldConfig => $Param{DynamicFieldConfig},
+        PossibleValues     => $PossibleValues,
+        Value              => $Value,
+    );
+
+    # check and set class if necessary
+    my $FieldClass = 'DynamicFieldCheckbox';
+    if ( defined $Param{Class} && $Param{Class} ne '' ) {
+        $FieldClass .= ' ' . $Param{Class};
+    }
+
+    # set field as mandatory
+    if ( $Param{Mandatory} ) {
+        $FieldClass .= ' Validate_Required';
+    }
+
+    # set error css class
+    if ( $Param{ServerError} ) {
+        $FieldClass .= ' ServerError';
+    }
+
+    my $FieldLabelEscaped = $Param{LayoutObject}->Ascii2Html(
+        Text => $FieldLabel,
+    );
+
+    my $HTMLString = '';
+    for my $BoxValue ( sort { $DataValues->{$a} cmp $DataValues->{$b} } keys %{ $DataValues || {} } ) {
+        my $BoxLabel     = $DataValues->{$BoxValue};
+        my $FieldChecked = $SelectedValues{$BoxValue} ? 'checked="checked"' : '';
+        $HTMLString .= qq~<input type="checkbox" class="$FieldClass" id="$FieldName" name="$FieldName" title="$FieldLabelEscaped" $FieldChecked value="$BoxValue" /> $BoxLabel<br />~;
+    }
+
+    # call EditLabelRender on the common Driver
+    my $LabelString = $Self->EditLabelRender(
+        %Param,
+        Mandatory => $Param{Mandatory} || '0',
+        FieldName => $FieldName,
+    );
+
+    my $Data = {
+        Field => $HTMLString,
+        Label => $LabelString,
+    };
+
+    return $Data;
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
